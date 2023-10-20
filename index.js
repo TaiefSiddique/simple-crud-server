9867eaa614b7d5722218cd2195a7fcd552d7be0e
TaiefSiddique <taiefsiddik2@gmail.com>
2023-10-20 21:48:29 +0600

    insertion functionalities added

--- index.js
+++ index.js
@@ -28,13 +28,18 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const database = client.db("insertDB");
+            const userData = database.collection("userData");
+            const result = await userData.insertOne(user);
+            res.send(result);
+        });
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
         //await client.close();
     }
 }
